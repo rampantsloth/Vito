790ee705f0953868d30c8cac3b20c0faca48956a
Rampant Sloth <mauricedemulder@gmail.com>
2011-10-05 23:22:52 +0200

    gui window template

---
+++ gui/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+#include <iostream>
+void MainWindow::on_actionOpen_Database_triggered()
+{
+}
