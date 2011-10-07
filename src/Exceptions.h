e4611215651e4db45356752b56d583e332359406
Rampant Sloth <mauricedemulder@gmail.com>
2011-10-07 10:57:36 +0200

    added Exceptions

---
+++ src/Exceptions.h
@@ -0,0 +1,29 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <exception>
+
+namespace vito{
+namespace exception{
+
+#define exc(name, parent, dummy)		\
+  class name : public parent{			\
+  virtual const char* what() const throw()	\
+  {						\
+    return dummy;				\
+  }						\
+  };						\
+
+exc(Vito, std::exception, "general exception");
+exc(Cache, Vito, "Something to do with the cache");
+exc(CacheReadingError, Cache, "Could not read an element from the Cache");
+exc(Plugins, Vito, "Problem to do with plugins");
+exc(CouldNotInstantiateProblem, Plugins, "Could not instantiate a certain plugin");
+exc(FileSystem, Vito, "Something to do with the files");
+exc(FileNotFound, FileSystem, "a certain file has not been found");
+exc(XML, FileSystem, "parameters node was not found inside the xml")
+} // exceptions
+}// vito
+
+
+#endif
