b21f4a6a4961216be56f35c2a0dea1037ae5d557
Rampant Sloth <mauricedemulder@gmail.com>
2011-10-07 10:58:32 +0200

    complete feature factory infrastructure

---
+++ src/CachedFeatureFetcher.h
@@ -0,0 +1,26 @@
+#ifndef CACHED_FEATURE_EXTRACTOR_H
+#define CACHED_FEATURE_EXTRACTOR_H
+
+#include "IVisualFeatureFetcher.h"
+#include "IVisualFeatureExtractor.h"
+#include "LRUCache.h"
+
+
+namespace vito{
+namespace features{
+
+class CachedFeatureFetcher : public IVisualFeatureFetcher{
+  IVisualFeatureExtractor::ptr extractor;
+  LRUCache<Descriptor::ptr> cache;
+public:
+  CachedFeatureFetcher(IVisualFeatureExtractor::ptr ext):
+    extractor(ext), cache(100){
+  }
+  virtual Descriptor::const_ptr extract(const DataPoint &dp, 
+					RgbImage *visrep = 0);
+};
+
+} // features
+} // vito
+
+#endif
